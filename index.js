6584e33acf8c89227e44eac4497c046c3ec710fd
Kamil Holub <kahol20000@gmail.com>
2023-05-18 21:44:58 -0400

    completed activity

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+move(character).moveWithArrowKeys(100, 250)
 
 
 move(newImage('assets/tree.png')).to(200, 450)
@@ -13,3 +13,5 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
--- move.js
+++ move.js
@@ -5,8 +5,72 @@ function move(element) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    function moveWithArrowKeys(left,bottom)
+    {
+            let x = 100;
+let y = 250;
+let direction = null;
+let vel = 4;
+document.addEventListener('keydown',function(event)
+{
+    if(event.repeat) return;
+
+    if(event.key === 'w')
+    {
+        character.src='assets/green-character/north.gif'
+        direction = '1'
+    }
+    if(event.key === 'a')
+    {
+        character.src='assets/green-character/west.gif'
+        direction = '2'
+    }
+    if(event.key === 's')
+    {
+        character.src='assets/green-character/south.gif'
+        direction = '3'
+    }
+    if(event.key === 'd')
+    {
+        character.src='assets/green-character/east.gif'
+        direction = '4'
+    }
+
+    
+    console.log(direction)
+
+})
+document.addEventListener('keyup',function(event){
+    direction=null;
+    character.src='assets/green-character/static.gif'
+})
+function movecharacter(){
+    if(direction === '1'){
+        
+        y=y+1
+    }
+    if(direction === '2')
+    {
+        x=x-1
+    }
+    if(direction==='3')
+    {
+        y=y-1
+    }
+    if(direction==='4')
+    {
+        x=x+1
+    }
+    character.style.bottom = y+'px'
+    character.style.left = x+'px'
+}
+
+setInterval(movecharacter, 1)
+    }
 
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        moveWithArrowKeys:moveWithArrowKeys
     }
+    
 }
